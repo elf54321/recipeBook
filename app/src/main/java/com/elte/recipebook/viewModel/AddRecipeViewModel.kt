7f9adc85295c0f7f1ba980c5d2700ea456e5bd39
Mohnansky Rudolf <rudolf23.hkya@gmail.com>
2025-05-09 04:04:25 +0200

    viewModelCleanup

--- app/src/main/java/com/elte/recipebook/viewModel/AddRecipeViewModel.kt
+++ app/src/main/java/com/elte/recipebook/viewModel/AddRecipeViewModel.kt
@@ -24,27 +24,56 @@ import javax.inject.Inject
 class AddRecipeViewModel @Inject constructor(
     private val recipeDao: RecipeDao
 ) : ViewModel() {
-    // Event channel
-    private val _eventFlow = MutableSharedFlow<UIEvent>()
-    val eventFlow = _eventFlow.asSharedFlow()
+    // --------------------------------------------------------------------------//
+    //                          Nutrition Part                                   //
+    // --------------------------------------------------------------------------//
 
-    sealed class UIEvent {
-        data class ShowSnackbar(val message: String): UIEvent()
-    }
 
-    // — UI state, backed by Compose’s mutableStateOf
-        var name by mutableStateOf("")
-            private set
 
-        var description by mutableStateOf("")
-            private set
 
-        var portionText by mutableStateOf("")    // collect as String, convert later
-            private set
+    // --------------------------------------------------------------------------//
 
-        // image URI string
-        private var imageUri by mutableStateOf<String?>(null)
+    // --------------------------------------------------------------------------//
+    //                          Recipe Part                                      //
+    // --------------------------------------------------------------------------//
 
+        // Event channel
+        private val _eventFlow = MutableSharedFlow<UIEvent>()
+        val eventFlow = _eventFlow.asSharedFlow()
+
+        sealed class UIEvent {
+            data class ShowSnackbar(val message: String): UIEvent()
+        }
+
+        ////////// AddRecipeScreen(1st) //////////
+
+            // — UI state, backed by Compose’s mutableStateOf
+            var name by mutableStateOf("")
+                private set
+
+            var description by mutableStateOf("")
+                private set
+
+            var portionText by mutableStateOf("")    // collect as String, convert later
+                private set
+
+            // image URI string
+            private var imageUri by mutableStateOf<String?>(null)
+                val imageUriString: String? get() = imageUri
+
+            // — UI event handlers
+            fun onNameChange(new: String)         { name = new }
+            fun onDescriptionChange(new: String) { description = new }
+            fun onImageSelected(uri: Uri?) { imageUri = uri?.toString() }
+
+            private fun resetForm() {
+                name         = ""
+                description = ""
+                imageUri     = null
+            }
+
+
+        ////////// AddRecipeDetailScreen(2nd) //////////
         // Backing state (private) + public getters for dropdowns
             private var _selectedType by mutableStateOf(TypeOfMeal.BREAKFAST)
             val selectedType: TypeOfMeal
@@ -58,83 +87,57 @@ class AddRecipeViewModel @Inject constructor(
             val selectedPriceCategory: PriceCategory
                 get() = _selectedPriceCategory
 
-        // Options for dropdowns
-            val availableTypes: List<TypeOfMeal>       = TypeOfMeal.entries
-            val availableEquipment: List<Equipment>    = Equipment.entries
-            val availablePriceCategories: List<PriceCategory> = PriceCategory.entries
+            // Options for dropdowns
+                val availableTypes: List<TypeOfMeal>       = TypeOfMeal.entries
+                val availableEquipment: List<Equipment>    = Equipment.entries
+                val availablePriceCategories: List<PriceCategory> = PriceCategory.entries
 
-    // — UI event handlers
-        fun onNameChange(new: String)         { name = new }
-        fun onDescriptionChange(new: String) { description = new }
-        fun onPortionChange(new: String)      { portionText = new }
-        fun onImageSelected(uri: Uri?) { imageUri = uri?.toString() }
 
-        // Handlers to update Dropdown states
-        fun onTypeSelected(type: TypeOfMeal) {
-            _selectedType = type
-        }
+            // — UI event handlers
+                fun onPortionChange(new: String)      { portionText = new }
 
-        fun toggleEquipment(eq: Equipment) {
-            if (_selectedEquipment.contains(eq)) {
-                _selectedEquipment.remove(eq)
-            } else if (_selectedEquipment.size < 12) {
-                _selectedEquipment.add(eq)
-                viewModelScope.launch {
-                    _eventFlow.emit(UIEvent.ShowSnackbar("Maximum 12 Equipment is allowed!"))
+                // Handlers to update Dropdown states
+                fun onTypeSelected(type: TypeOfMeal) {
+                    _selectedType = type
                 }
-            }
+                fun onPriceCategorySelected(pc: PriceCategory) {
+                    _selectedPriceCategory = pc
+                }
+
+                fun toggleEquipment(eq: Equipment) {
+                    if (_selectedEquipment.contains(eq)) {
+                        _selectedEquipment.remove(eq)
+                    } else if (_selectedEquipment.size < 12) {
+                        _selectedEquipment.add(eq)
+                        viewModelScope.launch {
+                            _eventFlow.emit(UIEvent.ShowSnackbar("Maximum 12 Equipment is allowed!"))
+                        }
+                    }
+                }
+                var isTypeMenuExpanded by mutableStateOf(false)
+                    private set
+                fun toggleTypeMenu() { isTypeMenuExpanded = !isTypeMenuExpanded }
+
+                var isPriceMenuExpanded by mutableStateOf(false)
+                    private set
+                fun togglePriceMenu() { isPriceMenuExpanded = !isPriceMenuExpanded }
+    // --------------------------------------------------------------------------//
+
+    // Creating a new recipe in the database
+    fun insertRecipe(onSuccess: () -> Unit = {}) {
+        // Validation
+        if (name.isBlank()) return
+
+        val newRecipe = Recipe(
+            name         = name.trim(),
+            description = description.trim(),
+            imageUri     = imageUri,
+        )
+
+        viewModelScope.launch {
+            recipeDao.insert(newRecipe)
+            resetForm()
+            onSuccess()
         }
-
-        fun onPriceCategorySelected(pc: PriceCategory) {
-            _selectedPriceCategory = pc
-        }
-
-    // — Insert to db
-        fun insertRecipe(onSuccess: () -> Unit = {}) {
-            // Validation
-            if (name.isBlank()) return
-
-            // val portion = portionText.toDoubleOrNull() ?: 1.0
-
-            val newRecipe = Recipe(
-                name         = name.trim(),
-                description = description.trim(),
-                imageUri     = imageUri,
-            )
-
-            viewModelScope.launch {
-                recipeDao.insert(newRecipe)
-                resetForm()
-                onSuccess()
-            }
-        }
-
-    /*
-    For adding Nutrition, Ingredient etc
-
-        fun onNutritionChange(updated: Nutrition) {
-        }
-        fun addIngredient(info: IngredientInformation) {
-        }
-        fun removeIngredient(info: IngredientInformation) {
-        }
-
-    */
-    // — Reset the form (Can be added as a button)
-        private fun resetForm() {
-            name         = ""
-            description = ""
-            imageUri     = null
-        }
-
-    var isTypeMenuExpanded by mutableStateOf(false)
-        private set
-    fun toggleTypeMenu() { isTypeMenuExpanded = !isTypeMenuExpanded }
-
-    var isPriceMenuExpanded by mutableStateOf(false)
-        private set
-    fun togglePriceMenu() { isPriceMenuExpanded = !isPriceMenuExpanded }
-
-    // Expose the image URI string:
-    val imageUriString: String? get() = imageUri
+    }
 }
